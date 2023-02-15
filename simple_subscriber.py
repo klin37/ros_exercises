f4df27993022efa8b7b34ba223fbb7322ad630c0
Katherine <katherinelin77@gmail.com>
2023-02-15 22:23:34 +0000

    finished simple subscriber

---
+++ simple_subscriber.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float32
+import numpy as np
+
+def callback(data):
+    pub = rospy.Publisher('random_float_log', Float32)
+    num = np.log(data.data)
+    rospy.loginfo(num)
+    pub.publish(num)
+
+def simple_subscriber():
+    rospy.init_node('simple_subscriber', anonymous=True)
+
+    rospy.Subscriber('my_random_float', Float32, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    simple_subscriber()
